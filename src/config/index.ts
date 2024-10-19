91418238e5ec14ba0c082ae8f3f4e773f121557f
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-10-19 18:11:48 +0600

    user interface added and validate

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,3 +7,4 @@ type Tenv = string | number | undefined;
 
 export const port: Tenv = process.env.PORT;
 export const db_uri: Tenv = process.env.DB_URI;
+export const bcrypt_salt_round: Tenv = process.env.BCRYPT_SALT_ROUND;
---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,23 @@
+import { Model } from 'mongoose';
+
+export interface TUser {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  role: 'admin' | 'investor';
+}
+
+export interface UserModel extends Model<TUser> {
+  //instance methods for checking if the user exist
+  isUserExistsByEmail(email: string): Promise<TUser>;
+  //instance methods for checking if passwords are matched
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+  isJWTIssuedBeforePasswordChanged(
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+  ): boolean;
+}
