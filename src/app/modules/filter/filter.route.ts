122523859b8febd481649c5205be661e2060e1a0
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-10-23 11:38:19 +0600

    jwt verify on all filter route

--- src/app/modules/filter/filter.route.ts
+++ src/app/modules/filter/filter.route.ts
@@ -6,14 +6,15 @@ import {
   filterOnOrderStatus,
   filterWhoGetOrder,
 } from './filter.controller';
+import { JwtVerify } from '../../middlewares/jwtVerify';
 
 const router: Router = Router();
 
 // all routes
-router.get('/filterByName/:name', filterByName); // filter by name
-router.get('/filterByOrder/:order', filterByTotalPrice); // filter by total price
-router.get('/filterByStatus/:status', filterOnOrderStatus); // filter on order status
-router.get('/filterByMoney/:moneyStatus', filterMoneyStatus); // filter on money status
-router.get('/whoGetTheOrder/:name', filterWhoGetOrder); // filter on who get order
+router.get('/filterByName/:name', JwtVerify, filterByName); // filter by name
+router.get('/filterByOrder/:order', JwtVerify, filterByTotalPrice); // filter by total price
+router.get('/filterByStatus/:status', JwtVerify, filterOnOrderStatus); // filter on order status
+router.get('/filterByMoney/:moneyStatus', JwtVerify, filterMoneyStatus); // filter on money status
+router.get('/whoGetTheOrder/:name', JwtVerify, filterWhoGetOrder); // filter on who get order
 
 export default router;
