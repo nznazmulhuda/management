7d2160462b7bbdb91696b0694d4b1091d198f7fd
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-11-10 23:53:42 +0600

    jwt update

--- src/app/middlewares/jwtVerify.ts
+++ src/app/middlewares/jwtVerify.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { jwt_secret_key } from '../config';
 
+// jwt verify
 export const JwtVerify = (
   req: Request,
   res: Response,
@@ -20,4 +21,32 @@ export const JwtVerify = (
 
     next();
   });
+};
+
+// jwt check
+export const JwtCheck = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): any => {
+  const { token } = req.cookies; // access token
+
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+
+  jwt.verify(token, jwt_secret_key as string, (err: any) => {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' });
+    }
+
+    next();
+  });
+};
+
+// jwt sign
+export const JwtSign = (payload: object): { token: string; data: object } => {
+  const token = jwt.sign(payload, jwt_secret_key as string);
+
+  return { token, data: payload };
 };
